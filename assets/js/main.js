f5559f69dc1ec74cbf628d49a961ec2e6c2fc026
Camilla Avila <avilacamilla@outlook.com>
2022-11-24 12:20:08 -0300

    Corrigindo título da folha de JS e adicionando comentários para organização

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+/* Página de Login */
+
+function logar() {
+
+    var email = document.getElementById('usuario').value;
+    var password = document.getElementById('password').value;
+
+    if (email == 'avilacamilla' && password == 'comunidadeCode') {
+        alert('Autenticado com sucesso!');
+        location.href = 'home.html';
+    } else {
+        alert('Usuário ou senha inválidos!');
+    }
+}
+
+/* Página home */
+
